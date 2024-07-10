6ee23d5bf2f6a6e5291e228fc6ac3110db6447ed
Murat Kuanishbaev <muratkuanysbaev2@gmail>
2024-07-10 13:56:28 +0500

    skeleton-in--products-list;

--- src/components/Pages/product-list.tsx
+++ src/components/Pages/product-list.tsx
@@ -1,11 +1,27 @@
-import { Flex } from "@mantine/core";
+import { Flex, Skeleton } from "@mantine/core";
 import useGetProducts from "../Hooks/use-get-products";
 import Product from "../Widgets/products";
 
 const ProductList = () => {
   const { products, isLoading } = useGetProducts();
+  const initialSkeleton = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-  if (!isLoading || !products) return <h1>Loading...</h1>;
+  if (!isLoading)
+    return (
+      <Flex
+        sx={{
+          flexWrap: "wrap",
+          justifyContent: "space-evenly",
+          rowGap: "30px",
+        }}
+      >
+        {initialSkeleton.map((item) => (
+          <Skeleton height={470} width={300} key={item} radius="md" />
+        ))}
+      </Flex>
+    );
+
+  if (!products) return <h1>Not products</h1>;
 
   return (
     <Flex
